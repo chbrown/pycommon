9a25bf7e1558d40ff1e672fc653457f9b95678d6
Christopher Brown <io@henrian.com>
2015-09-30 13:43:46 -0500

    Simplify operator overloading in AlgebraicDict

--- pycommon/algebraic.py
+++ pycommon/algebraic.py
@@ -1,6 +1,7 @@
 # importing division from __future__ affects only this module
 from __future__ import division
 import collections
+import operator
 
 
 class AlgebraicDict(collections.MutableMapping):
@@ -42,42 +43,49 @@ class AlgebraicDict(collections.MutableMapping):
 
     # add more functionality via operator overloads
 
+    def _apply_operator(self, op_func, rhs):
+        if isinstance(rhs, collections.MutableMapping):
+            return AlgebraicDict({key: op_func(self[key], rhs[key]) for key in self})
+        return AlgebraicDict({key: op_func(self[key], rhs) for key in self})
+
     def __add__(self, rhs):
         '''
         rhs can be another dict or AlgebraicDict, or a number
         '''
-        if isinstance(rhs, collections.MutableMapping):
-            return AlgebraicDict({key: self[key] + rhs[key] for key in self})
-        return AlgebraicDict({key: self[key] + rhs for key in self})
+        return self._apply_operator(operator.add, rhs)
+
+    def __sub__(self, rhs):
+        '''
+        rhs can be another dict or AlgebraicDict, or a number
+        '''
+        return self._apply_operator(operator.sub, rhs)
 
     def __mul__(self, rhs):
         '''
         rhs can be another dict or AlgebraicDict, or a number
         '''
-        if isinstance(rhs, collections.MutableMapping):
-            return AlgebraicDict({key: self[key] * rhs[key] for key in self})
-        return AlgebraicDict({key: self[key] * rhs for key in self})
+        return self._apply_operator(operator.mul, rhs)
 
     def __div__(self, rhs):
         '''
         rhs can be another dict or AlgebraicDict, or a number
         '''
-        if isinstance(rhs, collections.MutableMapping):
-            return AlgebraicDict({key: self[key] / rhs[key] for key in self})
-        return AlgebraicDict({key: self[key] / rhs for key in self})
+        return self._apply_operator(operator.truediv, rhs)
 
     def __truediv__(self, rhs):
         '''
-        __truediv__ is needed to make `normalized()` work when called from a
-        different module that does not import division from future.
+        rhs can be another dict or AlgebraicDict, or a number
         '''
-        return self.__div__(rhs)
+        return self._apply_operator(operator.truediv, rhs)
 
     def __pow__(self, rhs):
         '''
-        rhs should be a number
+        rhs can be another dict or AlgebraicDict, or a number
         '''
-        return AlgebraicDict({key: self[key] ** rhs for key in self})
+        return self._apply_operator(operator.pow, rhs)
+
+    def __abs__(self):
+        return AlgebraicDict({key: abs(value) for key, value in self.items()})
 
     # and a few more useful
 
