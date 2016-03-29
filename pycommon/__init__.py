87b2e3c9e1215f5bdc4ef5cdc790a038313a8d21
Christopher Brown <io@henrian.com>
2016-03-29 12:33:31 -0500

    Add several iterators/helpers to root module

--- pycommon/__init__.py
+++ pycommon/__init__.py
@@ -10,6 +10,24 @@ get0 = operator.itemgetter(0)
 get1 = operator.itemgetter(1)
 
 
+def lfind(xs, x):
+    '''
+    Find the index of the first (leftmost) instance of `x` in `xs` (a list).
+
+    Raises `ValueError` if `x` is not in `xs`
+    '''
+    return xs.index(x)
+
+
+def rfind(xs, x):
+    '''
+    Find the index of the last (rightmost) instance of `x` in `xs` (a list).
+
+    Raises `ValueError` if `x` is not in `xs`
+    '''
+    return (len(xs) - 1) - xs[::-1].index(x)
+
+
 def jaccard(A, B):
     '''
     Return the Jaccard similarity (the size of the intersection divided by the
@@ -28,6 +46,53 @@ def head(iterable, n=10):
     Take the first `n` entries from `iterable` and return them as a list.
     '''
     return list(itertools.islice(iterable, n))
+
+
+def window(iterable, n):
+    '''
+    Returns a sliding window (of width n) over data from the iterable
+        s -> (s0,s1,...s[n-1]), (s1,s2,...,sn), ...
+
+    If the desired window length is longer than iterable, yields nothing.
+
+    From https://docs.python.org/release/2.3.5/lib/itertools-example.html
+        via http://stackoverflow.com/a/6822773/424651
+    '''
+    iterable = iter(iterable)
+    # initialize with the first window
+    result = tuple(itertools.islice(iterable, n))
+    if len(result) == n:
+        yield result
+    # incrementally pop and push onto the result tuple for the rest of the items
+    for elem in iterable:
+        result = result[1:] + (elem,)
+        yield result
+
+
+def prefixes(xs):
+    '''
+    Iterate all prefixes of `xs`, starting with the empty slice, and ending
+    with `xs` itself.
+
+    >>> list(prefixes(('A', 'B', 'C')))
+    [(), ('A',), ('A', 'B'), ('A', 'B', 'C')]
+    '''
+    for i in range(len(xs) + 1):
+        yield xs[:i]
+
+
+def suffixes(xs):
+    '''
+    Iterate all suffixes of `xs`, starting with the empty slice, and ending
+    with `xs` itself.
+
+    Ordering is preserved.
+
+    list(suffixes(('A', 'B', 'C')))
+    >>> [(), ('C',), ('B', 'C'), ('A', 'B', 'C')]
+    '''
+    for i in range(len(xs) + 1):
+        yield xs[len(xs) - i:]
 
 
 def product(xs):
@@ -99,3 +164,12 @@ def groupby_select(iterable, keyfunc, valfunc):
     '''
     for key, sub_iterator in itertools.groupby(iterable, keyfunc):
         yield key, (valfunc(sub_item) for sub_item in sub_iterator)
+
+
+def strict_groupby(iterable, keyfunc):
+    '''
+    Like the standard itertools.groupby, but presorts iterable and evaluates
+    all iterators as lists.
+    '''
+    for key, sub_iterator in itertools.groupby(sorted(iterable, key=keyfunc), keyfunc):
+        yield key, list(sub_iterator)
