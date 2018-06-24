78880fb903a1f237388e3a124b969c185279a3d4
Christopher Brown <io@henrian.com>
2018-06-24 12:01:01 -0500

    Add basic fields to package def

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='pycommon',
     version='0.0.1',
     author='Christopher Brown',
     author_email='io@henrian.com',
+    description='General purpose Python programming modules',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/chbrown/pycommon',
     license='MIT',
-    packages=['pycommon']
+    packages=setuptools.find_packages(),
 )
